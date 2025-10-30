90437ec687cb8149ea68a7a0cdc9848749b7657c
Azza0411 <hamdi.azza@esprit.tn>
2025-10-30 23:38:06 +0100

    commit Template Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { TradingComponent } from './trading/trading.component';
+import { CommunauteComponent } from './communaute/communaute.component';
+import { FormationComponent } from './formation/formation.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'trading', component: TradingComponent },
+  { path: 'communaute', component: CommunauteComponent },
+  { path: 'formation', component: FormationComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' },
+  { path: '**', redirectTo: '/accueil' }
+];
